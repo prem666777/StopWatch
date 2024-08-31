f5e2189e4a326126bb2b9b9971959c3f1f1dc733
prem666777 <71005771+prem666777@users.noreply.github.com>
2024-09-01 03:19:46 +0530

    Frame created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        StopWatch stopwatch = new StopWatch();
+
+
+
+
 
     }
 }
--- src/StopWatch.java
+++ src/StopWatch.java
@@ -1,2 +1,45 @@
-public class StopWatch {
+import java.awt.event.*;
+import java.lang.*;
+import javax.swing.*;
+
+
+public class StopWatch implements ActionListener{
+
+    JFrame frame = new JFrame("Stopwatch");
+    JButton startButton = new JButton("Start");
+    JButton resetButton = new JButton("Reset");
+    JLabel timeLabel = new JLabel();
+    int elapsedTime = 0;
+    int seconds = 0;
+    int minutes = 0;
+    int hours = 0;
+
+    String seconds_string = String.format("%02d",seconds);
+    String minutes_string = String.format("%02d",minutes);
+    String hours_string = String.format("%02d",hours);
+
+    boolean started = false;
+
+    StopWatch(){
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    void start(){
+
+    }
+    void stop(){
+
+    }
+    void reset(){
+
+    }
 }
